6702332d88a64e60e88fffd3600be437000a3f94
Alejandro1603 <110935568+Alejandro1603@users.noreply.github.com>
2024-03-21 15:38:14 -0500

    Add files via upload
    comit

---
+++ inicio.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="inicio.html">Inicio</a></li>
+                <li><a href="nosotros.html">Sobre nosotros</a></li>
+                <li><a href="index.html">Contactanos</a></li>
+                <li><a href="login.html">Iniciar sesion</a></li>
+                <li><a href="register.html">Registrarse</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <h1>Bienvenido a nuestro sitio web</h1>
+        <p>Ya estas registrado correctamente</p>
+        <!-- Agrega aquí el contenido específico de la página de inicio -->
+    </main>
+    <footer>
+        <p>&copy; 2022 Mi sitio web. Todos los derechos reservados.</p>
+    </footer>
+</body>
+</html>
